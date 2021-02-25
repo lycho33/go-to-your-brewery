a647f0e1b8a9b8735a0b83a06105d87d2ab66679
lycho33 <cho.lyds@gmail.com>
2021-02-24 23:43:43 -0500

    added a method in Brewery class to find the city's breweries in the API

--- lib/brewery.rb
+++ lib/brewery.rb
@@ -21,6 +21,16 @@ class FINDBREWERY::Brewery
         self.all.select {|brewery| brewery.state == new_state}
     end
 
+    def self.find_or_create_by_city(city)
+        new_city = ""
+        if city.include?("%20")
+            new_city = city.split("%20").map {|x| x.capitalize}.join(" ")
+        else
+            new_city = city.capitalize 
+        end
+        self.all.select {|brewery| brewery.city == new_city}
+    end
+
     def self.all
         @@all
     end
